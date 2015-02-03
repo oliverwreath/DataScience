08b7141817e8ab51d5855cbcbf1036c0b635641e
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-03 15:24:02 +0800

    Create Gradient Descent in Python.py

---
+++ set3/Gradient Descent in Python.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas
+
+def compute_cost(features, values, theta): 
+	"""
+	Compute the cost of a list of parameters, theta, given a list of features 
+	(input data points) and values (output data points).
+	"""
+	m = len(values)
+	sum_of_square_errors = np.square(np.dot(features, theta) - values).sum()
+	cost = sum_of_square_errors / (2*m)
+
+	return cost
+
+def gradient_descent(features, values, theta, alpha, num_iterations): 
+	"""
+	Perform gradient descent given a data set with an arbitrary number of features.
+	"""
+
+	# Write code here that performs num_iterations updates to the elements of theta.
+	# times. Every time you compute the cost for a given list of thetas, append it 
+	# to cost_history.
+	# See the Instructor notes for hints. 
+	
+	cost_history = [] 
+	# print features.shape
+	# print features.shape[0] #1157
+	# print features.shape[1] #3
+	m = features.shape[0]
+	# print "features=", features
+	# print "values=", values
+	# print "theta=", theta
+	# print "alpha=", alpha
+	# print "num_iterations=", num_iterations
+
+	###########################
+	### YOUR CODE GOES HERE ###
+	###########################
+	for k in range(num_iterations): 
+		latest_cost = compute_cost(features, values, theta); 
+		cost_history.append(latest_cost)
+		# print "latest_cost=", latest_cost
+		# print "np.unique(np.dot(features, theta))=", np.unique(np.dot(features, theta)) 
+		# print "np.dot(features, theta)=", np.dot(features, theta)
+		# print "values - np.dot(features, theta)=", values - np.dot(features, theta)
+		# print "theta=", theta
+		theta2 = theta
+		theta[0] = theta2[0] + alpha/m * (values - np.dot(features, theta2)).sum()
+		for j in range(1, features.shape[1]): 
+			theta[j] = theta2[j] + alpha/m * ((values - np.dot(features, theta2)) * features[:,j]).sum()
+
+	return theta, pandas.Series(cost_history) # leave this line for the grader
+
+
+
