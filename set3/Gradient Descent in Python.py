31823bbcb87baaf9a75dcc14b58a461a57cda445
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-07 11:07:44 +0800

    Update Gradient Descent in Python.py

--- set3/Gradient Descent in Python.py
+++ set3/Gradient Descent in Python.py
@@ -1,155 +1,55 @@
 import numpy as np
 import pandas
-from ggplot import *
 
-"""
-In this question, you need to:
-1) implement the compute_cost() and gradient_descent() procedures
-2) Select features (in the predictions procedure) and make predictions.
-
-"""
-
-def normalize_features(df):
+def compute_cost(features, values, theta): 
 	"""
-	Normalize the features in the data set.
+	Compute the cost of a list of parameters, theta, given a list of features 
+	(input data points) and values (output data points).
 	"""
-	mu = df.mean()
-	sigma = df.std()
-	
-	if (sigma == 0).any():
-		raise Exception("One or more features had the same value for all samples, and thus could " + \
-						 "not be normalized. Please do not include features with only a single value " + \
-						 "in your model.")
-	df_normalized = (df - df.mean()) / df.std()
+	m = len(values)
+	sum_of_square_errors = np.square(np.dot(features, theta) - values).sum()
+	cost = sum_of_square_errors / (2*m)
 
-	return df_normalized, mu, sigma
-
-def compute_cost(features, values, theta):
-	"""
-	Compute the cost function given a set of features / values, 
-	and the values for our thetas.
-	
-	This can be the same code as the compute_cost function in the lesson #3 exercises,
-	but feel free to implement your own.
-	"""
-	
-	# your code here
-	cost = ((np.dot(features, theta) - values)**2).sum() / (2 * len(values))
 	return cost
 
-def gradient_descent(features, values, theta, alpha, num_iterations):
+def gradient_descent(features, values, theta, alpha, num_iterations): 
 	"""
 	Perform gradient descent given a data set with an arbitrary number of features.
-	
-	This can be the same gradient descent code as in the lesson #3 exercises,
-	but feel free to implement your own.
 	"""
-	
-	m = len(values)
-	cost_history = []
 
-	for k in range(num_iterations):
-		# your code here
-		cost = compute_cost(features, values, theta)
-		cost_history.append(cost)
-		theta2 = theta
-		theta[0] = theta2[0] + alpha/m * (values - np.dot(features, theta2)).sum()
-		for j in range(1, features.shape[1]):
-			theta[j] = theta2[j] + alpha/m * (values - np.dot(features, theta2) * features[:,j]).sum()
-	return theta, pandas.Series(cost_history)
+	# Write code here that performs num_iterations updates to the elements of theta.
+	# times. Every time you compute the cost for a given list of thetas, append it 
+	# to cost_history.
+	# See the Instructor notes for hints. 
+	
+	cost_history = [] 
+	# print features.shape
+	# print features.shape[0] #1157
+	# print features.shape[1] #3
+	m = features.shape[0]
+	# print "features=", features
+	# print "values=", values
+	# print "theta=", theta
+	# print "alpha=", alpha
+	# print "num_iterations=", num_iterations
 
-def predictions(dataframe):
-	'''
-	The NYC turnstile data is stored in a pandas dataframe called weather_turnstile.
-	Using the information stored in the dataframe, let's predict the ridership of
-	the NYC subway using linear regression with gradient descent.
-	
-	You can download the complete turnstile weather dataframe here:
-	https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv    
-	
-	Your prediction should have a R^2 value of 0.20 or better.
-	You need to experiment using various input features contained in the dataframe. 
-	We recommend that you don't use the EXITSn_hourly feature as an input to the 
-	linear model because we cannot use it as a predictor: we cannot use exits 
-	counts as a way to predict entry counts. 
-	
-	Note: Due to the memory and CPU limitation of our Amazon EC2 instance, we will
-	give you a random subet (~15%) of the data contained in 
-	turnstile_data_master_with_weather.csv. You are encouraged to experiment with 
-	this computer on your own computer, locally. 
-	
-	
-	If you'd like to view a plot of your cost history, uncomment the call to 
-	plot_cost_history below. The slowdown from plotting is significant, so if you 
-	are timing out, the first thing to do is to comment out the plot command again.
-	
-	If you receive a "server has encountered an error" message, that means you are 
-	hitting the 30-second limit that's placed on running your program. Try using a 
-	smaller number for num_iterations if that's the case.
-	
-	If you are using your own algorithm/models, see if you can optimize your code so 
-	that it runs faster.
-	'''
-	# Select Features (try different features!)
-	features = dataframe[['rain', 'precipi', 'Hour', 'meantempi']]
-	
-	# Add UNIT to features using dummy variables
-	dummy_units = pandas.get_dummies(dataframe['UNIT'], prefix='unit')
-	features = features.join(dummy_units)
-	
-	# Values
-	values = dataframe['ENTRIESn_hourly']
-	m = len(values)
+	###########################
+	### YOUR CODE GOES HERE ###
+	###########################
+	# print features.shape
+	# print len(theta)
+	# print range(1, features.shape[1])
+	for k in range(num_iterations): 
+		cost_history.append(compute_cost(features, values, theta))
+		# print "latest_cost=", latest_cost
+		# print "np.unique(np.dot(features, theta))=", np.unique(np.dot(features, theta)) 
+		# print "np.dot(features, theta)=", np.dot(features, theta)
+		# print "values - np.dot(features, theta)=", values - np.dot(features, theta)
+		# print "theta=", theta
+		errors = np.dot(features, theta)
+		theta = theta + alpha/m * np.dot((values - errors), features)
 
-	features, mu, sigma = normalize_features(features)
-	features['ones'] = np.ones(m) # Add a column of 1s (y intercept)
-	
-	# Convert features and values to numpy arrays
-	features_array = np.array(features)
-	values_array = np.array(values)
+	return theta, pandas.Series(cost_history) # leave this line for the grader
 
-	# Set values for alpha, number of iterations.
-	alpha = 0.1 # please feel free to change this value
-	num_iterations = 5 # please feel free to change this value
-
-	# Initialize theta, perform gradient descent
-	theta_gradient_descent = np.zeros(len(features.columns))
-	theta_gradient_descent, cost_history = gradient_descent(features_array, 
-															values_array, 
-															theta_gradient_descent, 
-															alpha, 
-															num_iterations)
-	
-	plot = None
-	# -------------------------------------------------
-	# Uncomment the next line to see your cost history
-	# -------------------------------------------------
-	# plot = plot_cost_history(alpha, cost_history)
-	# 
-	# Please note, there is a possibility that plotting
-	# this in addition to your calculation will exceed 
-	# the 30 second limit on the compute servers.
-	
-	predictions = np.dot(features_array, theta_gradient_descent)
-	return predictions, plot
-
-
-def plot_cost_history(alpha, cost_history):
-	"""This function is for viewing the plot of your cost history.
-	You can run it by uncommenting this
-
-		plot_cost_history(alpha, cost_history) 
-
-	call in predictions.
-	
-	If you want to run this locally, you should print the return value
-	from this function.
-	"""
-	cost_df = pandas.DataFrame({
-		'Cost_History': cost_history,
-		'Iteration': range(len(cost_history))
-	})
-	return ggplot(cost_df, aes('Iteration', 'Cost_History')) + \
-		geom_point() + ggtitle('Cost History for alpha = %.3f' % alpha )
 
 
