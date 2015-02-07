9f047212305be881570ee4a34db9184a535f439a
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-07 10:08:47 +0800

    Create aadhaar_generated_mapper.py

---
+++ Set5/aadhaar_generated_mapper.py
@@ -0,0 +1,44 @@
+import sys
+import string
+import logging
+
+from util import mapper_logfile
+logging.basicConfig(filename=mapper_logfile, format='%(message)s',
+					level=logging.INFO, filemode='w')
+
+def mapper():
+
+	#Also make sure to fill out the reducer code before clicking "Test Run" or "Submit".
+
+	#Each line will be a comma-separated list of values. The
+	#header row WILL be included. Tokenize each row using the 
+	#commas, and emit (i.e. print) a key-value pair containing the 
+	#district (not state) and Aadhaar generated, separated by a tab. 
+	#Skip rows without the correct number of tokens and also skip 
+	#the header row.
+
+	#You can see a copy of the the input Aadhaar data
+	#in the link below:
+	#https://www.dropbox.com/s/vn8t4uulbsfmalo/aadhaar_data.csv
+
+	#Since you are printing the output of your program, printing a debug 
+	#statement will interfere with the operation of the grader. Instead, 
+	#use the logging module, which we've configured to log to a file printed 
+	#when you click "Test Run". For example:
+	#logging.info("My debugging message")
+
+	word_counts = {}
+	sys.stdin.next()
+	for line in sys.stdin:
+		#your code here
+		data = line.strip().split(",")
+		# logging.info(len(data))
+		if len(data) == 12:
+			print data[3] + "\t" + data[8]
+			# logging.info(data[3] + "\t" + data[8])
+			# logging.info(data[3])
+			# logging.info(data[8])
+
+mapper()
+
+
